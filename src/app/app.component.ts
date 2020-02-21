209466ef2470cf69ba83d40db0fcbd81cceafda5
Dino Pascale <dino.pascale@gmail.com>
2020-02-21 01:29:27 +0100

    created functionality for countries api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountriesApiService } from 'src/api/countries.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'appName';
+export class AppComponent implements OnInit {
+  
+  constructor(private countriesApi: CountriesApiService) {}
+  
+  ngOnInit(): void {
+    this.countriesApi.getAllCountries({filters:['name', 'capital', 'currencies']}).subscribe(c => console.log('Countries', c))
+    this.countriesApi.getCountryByName({param: '/Italy', filters: ['name']}).subscribe(c => console.log('ITALIA', c))
+    this.countriesApi.getCountryByCode({param: '?codes=col;no;ee'}).subscribe(c => console.log('PROVIAMO ', c))
+  }
 }
